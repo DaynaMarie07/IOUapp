9d1353e8f5b0c51fc8b360101d5f676dfa11d0f2
dmarie <daynamarie@github.com>
2018-05-22 21:21:31 -0400

    heroku deploy3

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   port: 3306,
   host: "y5s2h87f6ur56vae.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "mpyx6ykl3srbbcls",
-  password: "fb12bbcn14wdn1on",
-  database: "y38klm1079irlwwz"
+  user: "c3798uag2jbi3ckx",
+  password: "u8pc4gr7g3b2h3u2",
+  database: "n14ojsu7zmm33u3u"
 });
 
 // Make connection.
